df86ed2254520016355af4e7391d82ad20d14893
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2016-03-03 02:18:18 +0900

    make submitButton disabled until setting a file

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,2 +1,8 @@
 $(function(){
+  $('#inputFile').on('change', function() {
+    var file = this.files[0];
+    if (file != null) {
+      $('#submitButton').prop("disabled", false);
+    }
+  });
 });
