89bdceca12dbb0fd5aa5c3b474002dc362a8a89d
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2016-03-06 18:28:38 +0900

    change from radio input to select input

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,8 +3,8 @@ $(function(){
   $form.find('#inputFile').on('change', function() {
     var file = this.files[0];
     if (file != null) {
-      var detectTypeCheck = $form.find('input[name=detectType]:checked').val();
-      if (detectTypeCheck) {
+      var detectTypeSelected = $form.find('#detectTypeSelectBox').val();
+      if (detectTypeSelected) {
         $('#submitButton').prop("disabled", false);
       }
     }
