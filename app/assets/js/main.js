c7a8ce58d0162db0637bf2d8efdefdf46e71e30d
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2016-03-06 20:12:58 +0900

    add help panel

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-  $form = $('#mainForm');
+  var $form = $('#mainForm');
   $form.find('#inputFile').on('change', function() {
     var file = this.files[0];
     if (file != null) {
@@ -9,4 +9,12 @@ $(function(){
       }
     }
   });
+
+  var $helpPanel = $('.helpPanel');
+  $helpPanel.find('a.help').on('click', function(e) {
+    e.preventDefault();
+
+    var $helpContent = $('.helpContent');
+    $helpContent.toggle();
+  })
 });
